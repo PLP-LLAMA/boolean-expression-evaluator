c06bcbbd61aa9b37b518f712a8cf0e1f6caab441
Wei Teng <weitengkoay@gmail.com>
2024-12-05 23:33:51 +0800

    enhancement: add custom error handler

--- boolean_expression.py
+++ boolean_expression.py
@@ -3,11 +3,16 @@
 from antlr4 import InputStream, CommonTokenStream
 from grammar.boolean_expression.BooleanExpressionLexer import BooleanExpressionLexer
 from grammar.boolean_expression.BooleanExpressionParser import BooleanExpressionParser
+from grammar.error_handlers.custom_error_listener import CustomErrorListener
 
 input_text = input("> ")
 lexer = BooleanExpressionLexer(InputStream(input_text))
+lexer.removeErrorListeners()
+lexer.addErrorListener(CustomErrorListener())
 stream = CommonTokenStream(lexer)
 parser = BooleanExpressionParser(stream)
+parser.removeErrorListeners()
+parser.addErrorListener(CustomErrorListener())
 
 tree = parser.program()
 
---
+++ grammar/error_handlers/custom_error_listener.py
@@ -0,0 +1,18 @@
+from antlr4.error.ErrorListener import ErrorListener
+
+class CustomErrorListener(ErrorListener):
+    def __init__(self):
+        super(CustomErrorListener, self).__init__()
+
+    def syntaxError(self, recognizer, offending_symbol, line, column, msg, e):
+        offending_symbol_text = (
+            offending_symbol.text if offending_symbol else "Unknown symbol"
+        )
+
+        print(
+            f"Syntax Error at line {line}, column {column}:\n"
+            f"  Message: {msg}\n"
+            f"  Offending Symbol: {offending_symbol_text}\n"
+            f"  Recognizer: {type(recognizer).__name__}\n"
+            f"  Exception: {str(e) if e else 'None'}"
+        )
