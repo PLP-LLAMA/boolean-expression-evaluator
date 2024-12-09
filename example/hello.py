3993665a9a38512349ba6eb4f611f6e99e9b312c
Adrian Teh <adrianteh02@hotmail.com>
2024-12-09 23:49:23 +0800

    add evaluator example - calculator

--- example/hello.py
+++ example/hello.py
@@ -1,4 +1,12 @@
+# example: hello
 # reference: https://yetanotherprogrammingblog.medium.com/antlr-with-python-974c756bdb1b
+
+import sys
+import os
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, parent_dir)
+
 
 from antlr4 import InputStream, CommonTokenStream
 from grammar.hello.HelloLexer import HelloLexer
